125adf28e86c022ca9ac2e462cc3ff2bfe9541d2
Lin Yem <linye@marianaocean.com>
2023-09-02 09:24:05 +0900

    Add d.ts

---
+++ dist/completion-generator.d.ts
@@ -0,0 +1,95 @@
+type LocaleDataItem = {
+    text: string;
+    keywords: string;
+};
+type MatchedResult = {
+    isMatched: boolean;
+    data?: MatchedResultData;
+};
+type MatchedResultData = {
+    text: string;
+    keywords: string;
+    matchedKeywords?: MatchedKeyword[];
+};
+type MatchedKeyword = {
+    text: string;
+    startAt: number;
+    endAt: number;
+};
+type CompletionGeneratorProperties = {
+    keywordSeparator: string;
+    minKeywordLength: number;
+    strictMatchLocales: string[];
+    comparator?: LocaleDataComparator;
+    filter?: LocaleDateFilter;
+};
+type LocaleDataComparator = (itemA: LocaleDataItem, itemB: LocaleDataItem, input: string, locale: string) => number;
+type LocaleDateFilter = (localeData: LocaleDataItem[], input: string, locale: string) => MatchedResultData[];
+interface CompletionGeneratorInter {
+    /** キーワードの分割文字 */
+    keywordSeparator: string;
+    /** キーワードとして認定する最短長さ */
+    minKeywordLength: number;
+    /** _strictMatchを使う言語コード設定 */
+    strictMatchLocales: string[];
+    /** ソート用メソッド */
+    comparator?: LocaleDataComparator;
+    /** 絞り込み用メソッド */
+    filter?: LocaleDateFilter;
+    data: Map<string, LocaleDataItem[]>;
+    loadData: (locale: string, localeData: LocaleDataItem[]) => void;
+    generateCompletions: (input: string, locale: string) => MatchedResultData[];
+}
+export declare class Generator implements CompletionGeneratorInter {
+    keywordSeparator: string;
+    minKeywordLength: number;
+    strictMatchLocales: string[];
+    comparator?: LocaleDataComparator;
+    filter?: LocaleDateFilter;
+    data: Map<string, LocaleDataItem[]>;
+    constructor(properties?: CompletionGeneratorProperties);
+    /**
+     * 候補データをインスタンスにセットする
+     * @param locale 言語コード
+     * @param localeData 言語データ
+     */
+    loadData(locale: string, localeData: LocaleDataItem[]): void;
+    _validateLocaleData(localeData: LocaleDataItem[]): void;
+    /**
+     * 指定入力テキストと言語に対し、補完データを生成して返す
+     * @param input 入力テキスト
+     * @param locale 言語コード
+     * @returns 補完データ
+     */
+    generateCompletions(input: string, locale: string): MatchedResultData[];
+    _getMatchedCompletions(localeData: LocaleDataItem[], input: string, locale: string): MatchedResultData[];
+    _match(dataItem: LocaleDataItem, input: string): MatchedResult;
+    _strictMatch(dataItem: LocaleDataItem, input: string): MatchedResult;
+}
+type CompletionFetcherProperties = {
+    apiKey: string;
+    apiKeyHeaderName?: string;
+    getEndpoint?: GetEndpoint;
+    handleResponse?: HandleResponse;
+};
+type GetEndpoint = (locale: string) => string;
+type HandleResponse = (data: any) => LocaleDataItem[];
+interface CompletionFetcherInter {
+    apiKey: string;
+    fetch(locale: string): Promise<LocaleDataItem[]>;
+}
+export declare class Fetcher implements CompletionFetcherInter {
+    apiKey: string;
+    apiKeyHeaderName: string;
+    getEndpoint: GetEndpoint;
+    handleResponse: HandleResponse;
+    constructor(properties?: CompletionFetcherProperties);
+    /**
+     * 指定エンドポイントから候補データを取得
+     * @param locale 言語コード
+     * @returns
+     */
+    fetch(locale: string): Promise<LocaleDataItem[]>;
+    _fetch(endpoint: string): Promise<any>;
+    isFetchAvailable(): boolean;
+}
